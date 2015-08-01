ed225cd9ee213ef11648ab0d047db3d984152e63
Avinash Soni <avinash.soni@gmail.com>
2015-08-01 15:56:19 +0530

    RDS

--- appink/settings.py
+++ appink/settings.py
@@ -90,11 +90,11 @@ WSGI_APPLICATION = 'appink.wsgi.application'
 DATABASES = {
   'default': {
   'ENGINE': 'django.db.backends.mysql',
-  'NAME': os.environ['RDS_DB_NAME'],
-  'USER': os.environ['RDS_USERNAME'],
-  'PASSWORD': os.environ['RDS_PASSWORD'],
-  'HOST': os.environ['RDS_HOSTNAME'],
-  'PORT': os.environ['RDS_PORT'],
+  'NAME': os.environ['pageinkdb'],
+  'USER': os.environ['pageinkuser'],
+  'PASSWORD': os.environ['Msmsgumsum8487'],
+  'HOST': os.environ['pageinkdbinstance.cqgkyept18if.us-west-2.rds.amazonaws.com'],
+  'PORT': os.environ['3306'],
   }
  }
 
